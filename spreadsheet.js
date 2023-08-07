c782bb7dc519651505cfddf2f6046c582e22335e
Bas Visscher <bas@koolecontrols.nl>
2023-08-07 11:26:36 +0200

    test

--- spreadsheet.js
+++ spreadsheet.js
@@ -10,31 +10,32 @@ const data = []
 document.addEventListener('DOMContentLoaded', init)
 const output = document.querySelector('.output')
 function init() {
-    fetch(url)
+    fetch(url, { mode: 'no-cors'})
         .then(res => res.text())
         .then(rep => {
             //Remove additional text and extract only JSON:
             const jsonData = JSON.parse(rep.substring(47).slice(0, -2));
-            console.log(rep);
+			
             const colz = [];
             const tr = document.createElement('tr');
             //Extract column labels
-            jsonData.table.cols.forEach((heading) => {
-                if (heading.label) {
-                    let column = heading.label;
-                    colz.push(column);
-                    const th = document.createElement('th');
-                    th.innerText = column;
-                    tr.appendChild(th);
-					
-					console.log(column);
-                }
-            })
+
+
+			colz.push("Name");
+			const th = document.createElement('th');
+			th.innerText = "Name";
+			tr.appendChild(th);
+			colz.push("Score");
+			const th2 = document.createElement('th');
+			th2.innerText = "Score";
+			tr.appendChild(th2);
+
+
             output.appendChild(tr);
             //extract row data:
             jsonData.table.rows.forEach((rowData) => {
 				
-					console.log(rowData);
+				//console.log(rowData);
                 const row = {};
                 colz.forEach((ele, ind) => {
                     row[ele] = (rowData.c[ind] != null) ? rowData.c[ind].v : '';
